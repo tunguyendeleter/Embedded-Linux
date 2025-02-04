fd6bd13dc8955c91857a1f33bc5d0658b1e33c92
Nguyen Ngoc Tu <80167731+tunguyendeleter@users.noreply.github.com>
2025-02-05 01:36:13 +0700

    Delete src directory

--- src/hello_ngoc.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "hello.h"
-
-void hello_Ngoc()
-{
-    printf("hello Ngoc\n");
-}
--- src/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "hello.h"
-
-int main()
-{
-    hello_Ngoc();
-    hello_Tu();
-    return 0;
-}
